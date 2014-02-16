530279e8e979d168fe2fffbb93f2826529642d24
HituziANDO <hhh.ando@gmail.com>
2014-02-16 17:45:40 +0900

    新規作成

---
+++ README.md
@@ -0,0 +1,32 @@
+GData Spreadsheet Sample
+======================
+Googleドライブにスプレッドシートを作成し,値の挿入(Insert),更新(Update),取得(Select),削除(Delete)をするサンプルです.  
+スプレッドシートは簡易的なデータベースとしても使うことができ,Googleアカウントがあれば誰でも利用することができます.  
+
+サンプルでは  
+1. Googleドライブ上に対象のスプレッドシートが無い場合はスプレッドシートを作成  
+2. 値の挿入  
+3. 値の更新  
+4. 値の取得  
+5. 値の削除  
+を順に実行します.
+
+依存関係
+----------
+以下のjarファイルが必要です.サンプルプロジェクトのlibsフォルダをコピーしてください.  
+
++ activation.jar
++ gdata-client-1.0.jar
++ gdata-client-meta-1.0.jar
++ gdata-core-1.0.jar
++ gdata-docs-3.0.jar
++ gdata-docs-meta-3.0.jar
++ gdata-media-1.0.jar
++ gdata-spreadsheet-3.0.jar
++ gdata-spreadsheet-meta-3.0.jar
++ guava-11.0.2.jar
++ jsr305.jar
++ mail.jar
+
+----------
+Copyright &copy; 2014 Hituzi ANDO
---
+++ src/Main.java
@@ -0,0 +1,104 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gdata.data.spreadsheet.CustomElementCollection;
+import com.google.gdata.data.spreadsheet.ListEntry;
+import com.google.gdata.util.ServiceException;
+
+import jp.hituzi.gdata.GoogleDrive;
+import jp.hituzi.gdata.Spreadsheet;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws ServiceException 
+	 * @throws IOException 
+	 */
+	public static void main (String[] args) throws IOException, ServiceException {
+		String appName = "jp.hituzi.gdata-Sample-1";
+		String userName = "*********@gmail.com";	// Your account name
+		String password = "*********";				// Your Password
+		String spreadsheetName = "jp_hituzi_gdata_Sampe";
+		
+		System.out.println("Access to Google Drive...");
+		
+		GoogleDrive drive = new GoogleDrive(appName, userName, password);
+		Spreadsheet spreadsheet = drive.getSpreadsheet();
+		if (drive.createSpreadsheetFile(spreadsheetName, false)) {
+			spreadsheet.chooseSpreadsheetByName(spreadsheetName);
+			spreadsheet.chooseDefaultWorksheet();
+			// Insert a header row.
+			spreadsheet.changeCells("A1:C1", new String[] { "col1", "col2", "col3" }, true);
+//			saveSpreadsheetKey(spreadsheet.getSpreadsheetKey());
+		} else {
+			spreadsheet.chooseSpreadsheetByName(spreadsheetName);
+//			String spreadsheetKey = loadSpreadsheetKey();
+//			spreadsheet.chooseSpreadsheetByKey(spreadsheetKey);
+			spreadsheet.chooseDefaultWorksheet();
+		}
+		
+		System.out.println("SpreadsheetName=" + spreadsheet.getSpreadsheetName());
+		System.out.println("SpreadsheetKey=" + spreadsheet.getSpreadsheetKey());
+		
+		System.out.println("Insert");
+		
+		Map<String, String> value1 = new HashMap<String, String>();
+		value1.put("col1", "hoge");
+		value1.put("col2", "foo");
+		value1.put("col3", "bar");
+		spreadsheet.insert(value1);
+		Map<String, String> value2 = new HashMap<String, String>();
+		value2.put("col1", "apple");
+		value2.put("col2", "orange");
+		value2.put("col3", "banana");
+		spreadsheet.insert(value2);
+		
+		printListEntries(spreadsheet.getEntries());
+		
+		System.out.println("Update");
+		
+		Map<String, String> value3 = new HashMap<String, String>();
+		value3.put("col1", "grape");
+		int count = spreadsheet.update(value3, "col1=apple");
+		
+		System.out.println("Updated! " + count);
+		
+		printListEntries(spreadsheet.getEntries());
+		
+		System.out.println("Select");
+		
+		List<ListEntry> list = spreadsheet.select("col1=grape");
+		printListEntries(list);
+		
+		System.out.println("Delete");
+		
+		count = spreadsheet.delete("col1=hoge");
+		
+		System.out.println("Deleted! " + count);
+		
+		printListEntries(spreadsheet.getEntries());
+	}
+
+//	private static void saveSpreadsheetKey (String key) {
+//		// TODO
+//	}
+
+//	private static String loadSpreadsheetKey () {
+//		// TODO
+//		return null;
+//	}
+
+	private static void printListEntries (List<ListEntry> entries) throws IOException, ServiceException {
+		for (ListEntry entry : entries) {
+			CustomElementCollection elem = entry.getCustomElements();
+			System.out.println("col1=" + elem.getValue("col1"));
+			System.out.println("col2=" + elem.getValue("col2"));
+			System.out.println("col3=" + elem.getValue("col3"));
+		}
+	}
+
+}
